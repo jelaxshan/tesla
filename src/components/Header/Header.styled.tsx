5b9e1b7ce9a4e327704a567ad7c3f592169961ec
jelaxshan <jelaxshan.jehanathan@ovoenergy.com>
2021-01-19 19:03:21 +0000

    Added desktop header nav

---
+++ src/components/Header/Header.styled.tsx
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+
+export const StyledNavBar = styled.div`
+  background-color: transparent;
+  display: flex;
+  padding: 16px;
+  justify-content: space-between;
+`;
+
+export const StyledNavLink = styled.div`
+  padding-right: 8px;
+  flex-direction: row;
+`;
+
+export const StyledNavToogle = styled.div`
+  display: flex;
+  margin-top: -2px;
+
+  ${({ theme: { mediaQueries } }) => `
+    ${mediaQueries.largeAndUp} {
+      display: none;
+      padding-right: 8px;
+      flex-direction: row;
+    }
+  `}
+`;
+
+export const StyledNavBarItems = styled.nav<{ open: boolean }>`
+  ${({ theme: { mediaQueries } }) => `
+    ${mediaQueries.largeAndUp} {
+     display: flex;
+     margin-top: 7px;
+     flex-direction: row;
+     position: static;
+    }
+  `}
+  display: none;
+`;
+
+export const StyledNavBarItemsRight = styled.nav<{ open: boolean }>`
+  ${({ theme: { mediaQueries } }) => `
+    ${mediaQueries.largeAndUp} {
+     display: flex;
+     margin-top: 7px;
+     flex-direction: row;
+    }
+  `}
+  display: none;
+`;
+
+export const StyledBurger = styled.button<{ open: boolean }>`
+  top: 5%;
+  left: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: black;
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => (open ? 'rotate(45deg)' : 'rotate(0)')};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? '0' : '1')};
+      transform: ${({ open }) => (open ? 'translateX(20px)' : 'translateX(0)')};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? 'rotate(-45deg)' : 'rotate(0)')};
+    }
+  }
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TeslaLogo from '../../assets/teslaLogoSmall.svg';
+import {
+  StyledNavBar,
+  StyledNavLink,
+  StyledNavBarItems,
+  StyledNavBarItemsRight,
+  StyledNavToogle,
+  StyledBurger,
+} from './Header.styled';
+
 function Header() {
-  return <header>Hello</header>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <StyledNavBar>
+      <StyledNavLink>
+        <img src={TeslaLogo} alt="Tesla logo" />
+      </StyledNavLink>
+      <StyledNavBarItems open={open}>
+        <StyledNavLink>MODEL S</StyledNavLink>
+        <StyledNavLink>MODEL 3</StyledNavLink>
+        <StyledNavLink>MODEL X</StyledNavLink>
+        <StyledNavLink>MODEL Y</StyledNavLink>
+        <StyledNavLink>POWERWALL</StyledNavLink>
+        <StyledNavLink>CHARGING</StyledNavLink>
+      </StyledNavBarItems>
+      <StyledNavBarItemsRight open={open}>
+        <StyledNavLink>SHOP</StyledNavLink>
+        <StyledNavLink>TESLA ACCOUNT</StyledNavLink>
+      </StyledNavBarItemsRight>
+      <StyledNavToogle>
+        <StyledBurger open={open} onClick={() => setOpen(!open)}>
+          <div />
+          <div />
+          <div />
+        </StyledBurger>
+      </StyledNavToogle>
+    </StyledNavBar>
+  );
 }
 
 export default Header;
